a6c767fa38b78efcabbeb20deb2a7064f4da35e7
technosophos <technosophos@bf33d5fa-052a-0410-b6c1-d98ccd4889b2>
2007-04-19 23:50:56 +0000

    Added a first draft of an enum for defining a Sinciput document
    git-svn-id: http://sinciput.googlecode.com/svn/trunk@25 bf33d5fa-052a-0410-b6c1-d98ccd4889b2

---
+++ sinciput/src/com/technosophos/sinciput/types/NotesEnum.java
@@ -0,0 +1,28 @@
+package com.technosophos.sinciput.types;
+
+public enum NotesEnum {
+	TYPE ("type", "Type"),
+	TITLE ("title", "Title"),
+	SUBTITLE ("subtitle", "Subtitle"),
+	CREATED_ON ("created_on", "Created On"),
+	MODIFIED_ON ("last_modified", "Last Modified"),
+	CREATED_BY ("created_by","Created By"),
+	MODIFIED_BY ("modified_by", "Modified By");
+	
+	private final String key;
+	private final String pretty_name;
+	
+	NotesEnum(String key, String pretty_name) {
+		this.key = key;
+		this.pretty_name = pretty_name;
+	}
+	
+	public String getPrintableName() {
+		return this.pretty_name;
+	}
+	
+	public String getKey() {
+		return this.key;
+	}
+	
+}
